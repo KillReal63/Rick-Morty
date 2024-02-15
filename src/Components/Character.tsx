fbce74474d78d15306343d1b5bc1d3f21fc17525
KillReal63 <104447337+KillReal63@users.noreply.github.com>
2024-02-15 23:05:30 +0400

    add swiper

--- src/Components/Character.tsx
+++ src/Components/Character.tsx
@@ -2,6 +2,7 @@ import { useQuery } from "@apollo/client";
 import { MAIN_LIST } from "../Services/Queries";
 import chunk from "lodash.chunk";
 import { StatusIcon } from "../Assets/Icons/StatusIcon";
+import { Swiper, SwiperSlide } from "swiper/react";
 
 type Characters = {
   id: number;
@@ -20,36 +21,49 @@ const Character = () => {
 
   if (!data) return <div>Loading...</div>;
 
-  const charactersChunk = data && chunk(data.characters.results, 10);
+  //const charactersChunk = data && chunk(data.characters.results, 10);
 
   console.log(data);
 
-  return charactersChunk[0].map(
-    ({ image, name, status, species, location, episode, id }: Characters) => (
-      <div
-        key={id}
-        className="w-[600px] h-[220px] flex bg-character rounded-lg m-3"
-      >
-        <img src={image} className="rounded-l-lg" />
-        <div className="grid text-white p-3">
-          <p className="text-2xl">{name}</p>
-          <div className="flex items-center">
-            <StatusIcon status={status} />
-            <p>{status}</p>
-            <p className="mx-1">-</p>
-            <p>{species}</p>
-          </div>
-          <div>
-            <p className="text-text_gray">Последняя локация</p>
-            {location.name}
-          </div>
-          <div>
-            <p className="text-text_gray">Появление</p>
-            {episode[0].name}
-          </div>
-        </div>
-      </div>
-    )
+  return (
+    <Swiper>
+      {data &&
+        data.characters.results.map(
+          ({
+            image,
+            name,
+            status,
+            species,
+            location,
+            episode,
+            id,
+          }: Characters) => (
+            <SwiperSlide
+              key={id}
+              className="w-[600px] h-[220px] flex bg-character rounded-lg m-3"
+            >
+              <img src={image} className="rounded-l-lg" />
+              <div className="grid text-white p-3">
+                <p className="text-2xl">{name}</p>
+                <div className="flex items-center">
+                  <StatusIcon status={status} />
+                  <p>{status}</p>
+                  <p className="mx-1">-</p>
+                  <p>{species}</p>
+                </div>
+                <div>
+                  <p className="text-text_gray">Последняя локация</p>
+                  {location.name}
+                </div>
+                <div>
+                  <p className="text-text_gray">Появление</p>
+                  {episode[0].name}
+                </div>
+              </div>
+            </SwiperSlide>
+          )
+        )}
+    </Swiper>
   );
 };
 
